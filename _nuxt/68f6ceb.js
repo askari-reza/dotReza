7ea91efd6aace2a33596343dd47268129119a1fb
reza-askasri <reza-askasri@users.noreply.github.com>
2021-11-04 12:00:45 +0000

    deploy: b860fcd087091bcd98721f66778abe4ebec2b5db

--- _nuxt/68f6ceb.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{502:function(n,t,e){"use strict";e.r(t);var l={asyncData:function(n){return{slug:n.params.slug}}},r=e(37),component=Object(r.a)(l,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",{},[n._v(n._s(n.slug))])}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ _nuxt/static/1636027242/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636027242",layout:"default",error:a,state:{errorMessage:void 0,successMessage:a,snackbar:b,loading:b},serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"87abb855"}}}}(null,false));
